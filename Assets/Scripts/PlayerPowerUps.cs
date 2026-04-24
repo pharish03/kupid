5ef6efb22c989d3879e73abdda26731be73934ef
Samir Rahil <srahil@charlotte.edu>
2026-04-24 15:32:04 -0400

    Finished grappleHook powerup
    Also turned every powerup into a prefab

--- Assets/Scripts/PlayerPowerUps.cs
+++ Assets/Scripts/PlayerPowerUps.cs
@@ -7,6 +7,7 @@ public class PlayerPowerUps : MonoBehaviour
     private int ActiveAbility = 0;
     public PlayerMovement playerMovement;
     public GameObject shield;
+    public Grappling grappling;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,7 +25,7 @@ public class PlayerPowerUps : MonoBehaviour
                 ActiveAbility = 2;
                 Debug.Log("Shield PowerUp Activated!");
             }
-            else if (other.gameObject.name == "Grapple Hook")
+            else if (other.gameObject.name == "grappleHook")
             {
                 ActiveAbility = 3;
                 Debug.Log("Grapple Hook PowerUp Activated!");
@@ -35,7 +36,7 @@ public class PlayerPowerUps : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) // WE, yes WE, are all using if else statements FOREVER!
         {
             if (ActiveAbility == 1) // Speed
             {
@@ -52,6 +53,9 @@ public class PlayerPowerUps : MonoBehaviour
             else if (ActiveAbility == 3) // AWESOME AF GRAPPLE HOOK
             {
                 Debug.Log("Using Grapple Hook PowerUp!");
+                grappling.enabled = true;
+                StartCoroutine(UseGrapple());
+                ActiveAbility = 0;
                 // Implement grapple hook logic here
             }
         }
@@ -60,13 +64,20 @@ public class PlayerPowerUps : MonoBehaviour
     private IEnumerator ActivateSpeed()
     {
         playerMovement.speed *= 2f; 
-        yield return new WaitForSeconds(7f); // Duration of the speed boost
+        yield return new WaitForSeconds(5f); // Duration of the speed boost
         playerMovement.speed /= 2f; 
     }
     private IEnumerator ActivateShield()
     {
         shield.SetActive(true); // Activate the shield
-        yield return new WaitForSeconds(20f); // Duration of the shield
+        yield return new WaitForSeconds(5f); // Duration of the shield
         shield.SetActive(false); // Deactivate the shield
+    }
+
+    private IEnumerator UseGrapple() // Player uses grapple hook -> This function fires -> Player finishes using grapple hook -> Grapplehook disabled :D
+    {
+        yield return new WaitForSeconds(5f); 
+        grappling.enabled = false;
+   
     }
 }
