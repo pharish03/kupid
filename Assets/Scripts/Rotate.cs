ceb76d0bfece96828a2fb9eadb4678aeaa8f63d7
Samir8104 <102767683+Samir8104@users.noreply.github.com>
2026-05-08 15:24:07 -0400

    Squashed commit of the following:
    commit 5ef6efb22c989d3879e73abdda26731be73934ef
    Author: Samir Rahil <srahil@charlotte.edu>
    Date:   Fri Apr 24 15:32:04 2026 -0400
    
        Finished grappleHook powerup
    
        Also turned every powerup into a prefab
    
    commit 69f9d205c3fee24023687fd98055f78d74555df9
    Author: Samir8104 <102767683+Samir8104@users.noreply.github.com>
    Date:   Thu Apr 16 16:31:56 2026 -0400
    
        Added a grappling hook (WIP)
    
        I just need to add a powerup for it but yeah
    
    commit 05d499b93b4d9486fddadf67652a158aefa1a147
    Author: Samir8104 <102767683+Samir8104@users.noreply.github.com>
    Date:   Thu Apr 16 15:21:28 2026 -0400
    
        Added Shield (WIP)
    
        The shield works but the players arrow can break your own shield, I might fix it later
    
    commit 480c14bfaa1b3b3949b408c8de8951e3d94afcb0
    Author: Samir8104 <102767683+Samir8104@users.noreply.github.com>
    Date:   Thu Apr 16 14:27:06 2026 -0400
    
        Added Speed Powerup
    
        Also added the shield model

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public Vector3 rotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+}
